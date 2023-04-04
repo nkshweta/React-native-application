07354c82d5bfbf6df70c4b151aa8aab50c032d58
nkshweta <51898187+nkshweta@users.noreply.github.com>
2023-04-04 16:58:33 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native\
+ *
+ * @format
+ */
+import React from 'react';
+import RootNavigator from './src/navigation';
+
+export default function App() {
+  return (
+    <>
+      <RootNavigator />
+    </>
+  );
+}
